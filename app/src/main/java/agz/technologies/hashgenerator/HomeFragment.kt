2258a3234fe395c88f2d59923cffbf89f646f1c1
andre <andresro0198@gmail.com>
2021-06-06 20:20:46 +0200

    Transition animations applied

--- app/src/main/java/agz/technologies/hashgenerator/HomeFragment.kt
+++ app/src/main/java/agz/technologies/hashgenerator/HomeFragment.kt
@@ -7,6 +7,7 @@ import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
@@ -28,10 +29,7 @@ class HomeFragment : Fragment() {
         binding.autocompleteTextView.setAdapter(arrayAdapter)
 
         binding.generateButton.setOnClickListener {
-               lifecycleScope.launch{
-                   applyAnimations()
-                   navigateToSuccess()
-               }
+            onGenerateClicked()
         }
 
         return binding.root
@@ -41,12 +39,9 @@ class HomeFragment : Fragment() {
         inflater.inflate(R.menu.home_menu, menu)
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        _binding = null // we avoid memory leaks
-    }
 
-    private suspend fun applyAnimations(){
+    private suspend fun applyAnimations() {
+        binding.generateButton.isClickable = false
         binding.titleTextView.animate().alpha(0f).duration = 400L
         binding.generateButton.animate().alpha(0f).duration = 400L
         binding.textInputLayout.animate().alpha(0f).translationXBy(1200f).duration = 400L
@@ -66,7 +61,42 @@ class HomeFragment : Fragment() {
         delay(1500L)
     }
 
-    private fun navigateToSuccess(){
+    private fun navigateToSuccess() {
         findNavController().navigate(R.id.action_homeFragment_to_successFragment)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null // we avoid memory leaks
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val hashAlgorithms = resources.getStringArray(R.array.hash_algorithms)
+        val arrayAdapter = ArrayAdapter(requireContext(), R.layout.drop_down_item, hashAlgorithms)
+        binding.autocompleteTextView.setAdapter(arrayAdapter)
+    }
+
+    private fun onGenerateClicked() {
+        if (binding.plainText.text.isEmpty()) {
+            showSnackBar("Field is empty.")
+        } else {
+            lifecycleScope.launch {
+                applyAnimations()
+                navigateToSuccess()
+            }
+        }
+
+    }
+
+    private fun showSnackBar(message: String) {
+        val snackBar = Snackbar.make(
+            binding.rootLayout,
+            message,
+            Snackbar.LENGTH_LONG
+        )
+        snackBar.setAction("Okay") {}
+            snackBar.show()
+
     }
 }
